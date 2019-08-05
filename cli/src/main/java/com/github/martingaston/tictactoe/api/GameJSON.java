c321a51381055744ef6c4c844ab0d444f1ab1267
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-08-05 18:04:47 +0100

    add JSON encoder

--- cli/src/main/java/com/github/martingaston/tictactoe/api/GameJSON.java
+++ cli/src/main/java/com/github/martingaston/tictactoe/api/GameJSON.java
@@ -16,7 +16,7 @@ public class GameJSON {
     private Map<String, String> messages;
 
     @JsonCreator
-    private static GameJSON from(
+    public static GameJSON from(
             @JsonProperty("mode") String mode,
             @JsonProperty("currentPlayer") String currentPlayer,
             @JsonProperty("isActive") boolean isActive,
@@ -26,7 +26,7 @@ public class GameJSON {
         return new GameJSON(mode, currentPlayer, isActive, board, messages);
     }
 
-    private GameJSON(String mode, String currentPlayer, boolean isActive, ArrayList<String> board, HashMap<String, String> messages) {
+    private GameJSON(String mode, String currentPlayer, boolean isActive, List<String> board, Map<String, String> messages) {
         this.mode = mode;
         this.currentPlayer = currentPlayer;
         this.isActive = isActive;
@@ -34,22 +34,69 @@ public class GameJSON {
         this.messages = messages;
     }
 
+    public static class Builder {
+        private String mode;
+        private String currentPlayer;
+        private boolean isActive;
+        private List<String> board;
+        private Map<String, String> messages;
+
+        public Builder mode(String mode) {
+            this.mode = mode;
+
+            return this;
+        }
+
+        public Builder currentPlayer(String currentPlayer) {
+            this.currentPlayer = currentPlayer;
+
+            return this;
+        }
+
+        public Builder isActive(boolean isActive) {
+            this.isActive = isActive;
+
+            return this;
+        }
+
+        public Builder board(List<String> board) {
+            this.board = board;
+
+            return this;
+        }
+
+        public Builder messages(Map<String, String> messages) {
+            this.messages = messages;
+
+            return this;
+        }
+
+        public GameJSON build() {
+            return new GameJSON(this.mode, this.currentPlayer, this.isActive, this.board, this.messages);
+        }
+    }
+
+    @JsonProperty("isActive")
     public boolean isActive() {
         return this.isActive;
     }
 
+    @JsonProperty("mode")
     public String mode() {
         return this.mode;
     }
 
+    @JsonProperty("currentPlayer")
     public String currentPlayer() {
         return this.currentPlayer;
     }
 
+    @JsonProperty("board")
     public List<String> board() {
         return this.board;
     }
 
+    @JsonProperty("messages")
     public Map<String, String> messages() {
         return this.messages;
     }
---
+++ cli/src/test/java/com/github/martingaston/tictactoe/api/EncodeTest.java
@@ -0,0 +1,29 @@
+package com.github.martingaston.tictactoe.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class EncodeTest {
+   @Test
+   public void encodesGameJsonObject() throws JsonProcessingException {
+       GameJSON gameJson = new GameJSON.Builder()
+               .mode("ai")
+               .currentPlayer("O")
+               .isActive(false)
+               .board(new ArrayList<>(Arrays.asList("X", "O", "X", null, null, null, null, null, null)))
+               .messages(new HashMap<>())
+               .build();
+
+       String expected = "{\"board\":[\"X\",\"O\",\"X\",null,null,null,null,null,null],\"currentPlayer\":\"O\",\"isActive\":false,\"messages\":{},\"mode\":\"ai\"}";
+
+       String encoded = Encode.from(gameJson);
+
+       assertEquals(expected, encoded);
+   }
+}
