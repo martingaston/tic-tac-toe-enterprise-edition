486b6bcf0b7b26bcff88058c9cb48270ef5cde7b
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-08-07 18:17:54 +0100

    add IOException to functional interface

--- backend/src/main/java/com/github/martingaston/application/routes/Handler.java
+++ backend/src/main/java/com/github/martingaston/application/routes/Handler.java
@@ -3,7 +3,9 @@ package com.github.martingaston.application.routes;
 import com.github.martingaston.application.http.Request;
 import com.github.martingaston.application.http.Response;
 
+import java.io.IOException;
+
 @FunctionalInterface
 public interface Handler {
-    Response.Options handle(Request request, Response.Options response);
+    Response.Options handle(Request request, Response.Options response) throws IOException;
 }
